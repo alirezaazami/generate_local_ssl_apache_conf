9d765f8e4b8029402487163cacc28bf9a5d5e924
alirezaazami <31056961+alirezaazami@users.noreply.github.com>
2024-07-24 13:29:28 +0330

    Update switch_php.sh

--- switch_php.sh
+++ switch_php.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]; then
 fi
 
 php=${1}
-[ ! -f "/usr/bin/${php}" ] && echo "installing php ${php}"
+[ ! -f "/usr/bin/${php}" ] && echo "installing ${php}"
 
 if [ ! -f "/usr/bin/${php}" ]; then
     sudo apt install ${php} ${php}-xml ${php}-mysql ${php}-fpm ${php}-zip ${php}-soap ${php}-mongodb ${php}-mbstring ${php}-intl ${php}-gd ${php}-curl ${php}-bz2 ${php}-xdebug ${php}-gmp
